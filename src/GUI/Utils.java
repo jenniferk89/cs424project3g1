35400af6d4b2209af4fb2344e9453d13e3e1a2c0
Joshua Herman <zitterbewegung@gmail.com>
2011-10-28 12:32:09 -0500

    Added haversine fornula

--- src/GUI/Utils.java
+++ src/GUI/Utils.java
@@ -37,6 +37,14 @@ public class Utils {
 	public static final int WEEK = 0;
 	public static final int MONTH = 0;
 	public static final int YEAR = 0;
+    //From http://en.wikipedia.org/wiki/Haversine_formula
+    public double haversine(double lat1, double lng1, double lat2, double lng2) {
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lng2 - lng1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(Math.toRadians(lat1)) * 
+        Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        return 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)) * 6371; // 6371 = average earth radius
+    }
 	
 	
 }
