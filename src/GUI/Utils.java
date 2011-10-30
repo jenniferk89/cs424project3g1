744a8fb6c999f9a76e42c5062e979c7820821864
Andrea Cazzola <cazzola.andrea@gmail.com>
2011-10-30 16:56:46 -0500

    Revert "modified setup phase to accept more input files"
    This reverts commit 1f7fbda496167e06c7e99c54daaef3b9b9d66db2.

--- src/GUI/Utils.java
+++ src/GUI/Utils.java
@@ -2,8 +2,6 @@ package GUI;
 
 import java.util.ArrayList;
 
-import com.modestmaps.InteractiveMap;
-
 import control.*;
 
 
@@ -15,7 +13,6 @@ import processing.core.PApplet;
 
 public class Utils {
 	public static PApplet globalProcessing;
-	public static InteractiveMap globalMap;
 	public static ArrayList<Sighting> allSightings;
 	public static ArrayList<Location> allLocations;
 	public static ArrayList<State> allStates;
@@ -37,12 +34,9 @@ public class Utils {
 	public int timeAggregationLevel;
 	//time different levels
 	public static final int DAY = 0;
-	public static final int WEEK = 1;
-	public static final int MONTH = 2;
-	public static final int YEAR = 3;
-	
-	
-	
+	public static final int WEEK = 0;
+	public static final int MONTH = 0;
+	public static final int YEAR = 0;
 	
 	
 }
